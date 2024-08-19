7fa7ac0aae6fa8c4808f949025792117755b64ac
GayathriMani12 <gayathrigayu505@gmail.com>
2024-08-19 14:57:39 +0530

    addd two methods

--- src/test/java/org/git/Login.java
+++ src/test/java/org/git/Login.java
@@ -34,4 +34,12 @@ public class Login {
 		// TODO Auto-generated method stub
 
 	}
+	private void method8() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method9() {
+		// TODO Auto-generated method stub
+
+	}
 }
